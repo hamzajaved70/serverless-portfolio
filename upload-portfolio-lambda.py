f3015755da9e6db778d5211f05ea3f3b78955ac7
Hamza Javed <hamzajaved70@gmail.com>
2020-02-11 01:24:38 +1100

    Added upload automation

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -13,5 +13,5 @@ build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
 with zipfile.ZipFile(portfolio_zip) as myzip:
     for name in myzip.namelist():
         obj = myzip.open(name)
-        portfolio_bucket.upload_fileobj(obj, name, ExtraArgs={'ContentType': mimetypes.guess(name)[0]})
+        portfolio_bucket.upload_fileobj(obj, name, ExtraArgs={'ContentType': mimetypes.guess_type(name)[0]})
         portfolio_bucket.Object(name).Acl().put(ACL='public-read')

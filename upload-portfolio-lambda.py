cd7264e4a1e38800e7dbd005ce21c18ae7cfb913
Hamza Javed <hamzajaved70@gmail.com>
2020-02-11 02:04:44 +1100

    converted to lambda format

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -1,17 +1,25 @@
+import json
 import boto3
 import zipfile
 import io
 import mimetypes
 
-s3 = boto3.resource('s3')
-portfolio_bucket = s3.Bucket('portfolio.hamzajav.com')
-build_bucket = s3.Bucket('portfoliobuild.hamzajav.com')
+def lambda_handler(event, context):
 
-portfolio_zip = io.BytesIO()
-build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+    s3 = boto3.resource('s3')
+    portfolio_bucket = s3.Bucket('portfolio.hamzajav.com')
+    build_bucket = s3.Bucket('portfoliobuild.hamzajav.com')
 
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for name in myzip.namelist():
-        obj = myzip.open(name)
-        portfolio_bucket.upload_fileobj(obj, name, ExtraArgs={'ContentType': mimetypes.guess_type(name)[0]})
-        portfolio_bucket.Object(name).Acl().put(ACL='public-read')
+    portfolio_zip = io.BytesIO()
+    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for name in myzip.namelist():
+            obj = myzip.open(name)
+            portfolio_bucket.upload_fileobj(obj, name, ExtraArgs={'ContentType': mimetypes.guess_type(name)[0]})
+            portfolio_bucket.Object(name).Acl().put(ACL='public-read')
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
